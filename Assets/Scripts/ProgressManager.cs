e4c74f20ff0cc032f4a129a23d5554a85990094f
cmont13 <cmontie0922@gmail.com>
2024-04-11 14:34:49 -0400

    Added animated door

--- Assets/Scripts/ProgressManager.cs
+++ Assets/Scripts/ProgressManager.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 public class ProgressManager : MonoBehaviour
 {
     private bool isPowerOn;
+    private Animator rightDoorAnimator;
+    private Animator leftDoorAnimator;
     [SerializeField] private int fuseCount;
     [SerializeField] private GameObject monster;
-
+    [SerializeField] private GameObject rightDoor;
+    [SerializeField] private GameObject leftDoor;
     public int FuseCount { get => this.fuseCount; set => this.fuseCount = value; }
     public Progress Progress { get; set; }
     public event Action LoadGame;
@@ -44,6 +47,8 @@ public class ProgressManager : MonoBehaviour
         RenderSettings.ambientLight = new Color32(55, 55, 55, 0);
         Destroy(monster);
         AudioManager.Instance.StopMusic();
+        rightDoorAnimator.Play("DoorOpenRight");
+        leftDoorAnimator.Play("DoorOpenLeft");
     }
 
     private void LoadMonster()
@@ -59,6 +64,8 @@ public class ProgressManager : MonoBehaviour
 
     private void Start()
     {
+        rightDoorAnimator = rightDoor.GetComponent<Animator>();
+        leftDoorAnimator = leftDoor.GetComponent<Animator>();
         AudioManager.Instance.PlayMusic("Section 1 Horror");
         SaveManager.Instance.LoadProgress();
         LoadGame += LoadMonster;
