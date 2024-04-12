430df19dc65f88e3f182b6e27d8b09ad48bed7cf
Tyler Knoop <MonotudeBusiness@gmail.com>
2024-04-11 23:13:42 -0400

    roam completed

--- Assets/Scripts/Monster 2/States/RoamState.cs
+++ Assets/Scripts/Monster 2/States/RoamState.cs
@@ -1,15 +1,42 @@
 using System;
+using UnityEngine;
 
 [Serializable]
 public class RoamState : State
 {
-    public override void Action(StateMachine monsterStateMachine)
-    {
+    [SerializeField] private float roamSpeed;
+    [SerializeField] private Transform[] destinations;
+    [Range(2f, 5f)]
+    [SerializeField] private float destinationAccuracy;
+    private Vector3 currentDestination;
 
+    private Vector3 GetDestination()
+    {
+        Vector3 destination = destinations[UnityEngine.Random.Range(0, destinations.Length)].position;
+
+        while (destination == currentDestination)
+        {
+            destination = destinations[UnityEngine.Random.Range(0, destinations.Length)].position;
+        }
+
+        return destination;
     }
 
-    public override void EnterState(StateMachine monsterStateMachine)
+    public override void EnterState(StateMachine stateMachine)
     {
+        stateMachine.NavMeshAgent.speed = roamSpeed;
+        currentDestination = GetDestination();
+        stateMachine.NavMeshAgent.destination = currentDestination;
+    }
 
+    public override void Action(StateMachine stateMachine)
+    {
+        Vector3 distanceVector = stateMachine.NavMeshAgent.transform.position - currentDestination;
+
+        if (distanceVector.magnitude <= destinationAccuracy)
+        {
+            currentDestination = GetDestination();
+            stateMachine.NavMeshAgent.destination = currentDestination;
+        }
     }
 }
