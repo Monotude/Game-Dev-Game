ea45f0e02189defdf094427ad2cebdf4f70e99bc
Tyler Knoop <MonotudeBusiness@gmail.com>
2024-04-16 19:46:07 -0400

    small interact prompt addition

--- Assets/Scripts/Player/PlayerInteract.cs
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
-    private Transform mainCamera;
+    [SerializeField] private GameObject interactPrompt;
     [Range(0.05f, 0.25f)]
     [SerializeField] private float interactAccuracy;
     [SerializeField] private float interactRange;
+    private Transform mainCamera;
 
     private void Start()
     {
@@ -14,18 +15,25 @@ public class PlayerInteract : MonoBehaviour
 
     private void Update()
     {
-        if (!InputManager.Instance.InteractButtonDown)
-        {
-            return;
-        }
-
         Ray ray = new Ray(mainCamera.position, mainCamera.forward);
         if (!Physics.SphereCast(ray, interactAccuracy, out RaycastHit hit, interactRange))
         {
+            interactPrompt.SetActive(false);
             return;
         }
 
         if (hit.collider.gameObject.TryGetComponent(out IInteractable interactable))
+        {
+            interactPrompt.SetActive(true);
+        }
+
+        else
+        {
+            interactPrompt.SetActive(false);
+            return;
+        }
+
+        if(InputManager.Instance.InteractButtonDown)
         {
             interactable.Interact();
         }
