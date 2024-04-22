eea23ae100ae2229ced882224fe67d1715d32272
Tyler Knoop <MonotudeBusiness@gmail.com>
2024-04-21 20:23:07 -0400

    Part 1

--- Assets/Scripts/Decals/UVShaderInteraction.cs
+++ Assets/Scripts/Decals/UVShaderInteraction.cs
@@ -4,15 +4,19 @@ public class UVShaderInteraction : MonoBehaviour
 {
     [SerializeField] private PlayerUVLight playerUVLight;
     [SerializeField] private Material material;
+    private UnityEngine.Rendering.Universal.DecalProjector decal;
+    private float isUvOn;
 
     void Start()
     {
+        decal = gameObject.GetComponent<UnityEngine.Rendering.Universal.DecalProjector>();
         material.SetFloat("_IsUvLightOn", 0.0f);
     }
 
     void Update()
     {
-        float isUvOn = playerUVLight.IsUVLightOn ? 1.0f : 0.0f;
+        isUvOn = playerUVLight.IsUVLightOn? 1.0f : 0.0f;
+        decal.drawDistance = playerUVLight.IsUVLightOn? 7.0f : 1000.0f;
         material.SetFloat("_IsUvLightOn", isUvOn);
     }
 }
