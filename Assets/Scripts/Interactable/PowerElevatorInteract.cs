da8f623142bf0a468f4b18218c38d0b696f792bb
Tyler Knoop <MonotudeBusiness@gmail.com>
2024-04-24 21:14:55 -0400

    Change

--- Assets/Scripts/Interactable/PowerElevatorInteract.cs
+++ Assets/Scripts/Interactable/PowerElevatorInteract.cs
@@ -5,6 +5,10 @@ public class PowerElevatorInteract : MonoBehaviour, IInteractable
     [SerializeField] private OpenElevatorDoors openElevatorDoors;
     [SerializeField] private GameObject theGang;
     [SerializeField] private Material material;
+    [SerializeField] private Animator section1DoorLeft;
+    [SerializeField] private Animator section1DoorRight;
+    [SerializeField] private Animator section2DoorLeft;
+    [SerializeField] private Animator section2DoorRight;
 
     private void Start()
     {
@@ -13,9 +17,17 @@ public class PowerElevatorInteract : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        material.color = Color.green;
-        openElevatorDoors.StartElevatorProgress();
-        theGang.SetActive(true);
-        RenderSettings.ambientLight = new Color(0, 0, 0, 0);
+        if (!theGang.activeSelf)
+        {
+            material.color = Color.green;
+            openElevatorDoors.StartElevatorProgress();
+            theGang.SetActive(true);
+            RenderSettings.ambientLight = new Color(0, 0, 0, 0);
+            AudioManager.Instance.PlayMusic("Section 1 Horror");
+            section1DoorLeft.Play("DoorCloseLeft");
+            section1DoorRight.Play("DoorCloseRight");
+            section2DoorLeft.Play("DoorCloseLeft");
+            section2DoorRight.Play("DoorCloseRight");
+        }
     }
 }
